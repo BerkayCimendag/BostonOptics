94538071bef576c5843a77f32e021934294e9337
BerkayCimendag <cimendagberkay@gmail.com>
2023-02-24 17:36:28 +0300

    add item to basket navbasket

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -6,6 +6,7 @@ global using ApplicationCore.Interfaces;
 global using ApplicationCore.Entities;
 using Web.Interfaces;
 using Web.Services;
+using ApplicationCore.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +19,8 @@ builder.Services.AddDbContext<ShopContext>(options => options.UseNpgsql(builder.
 
 builder.Services.AddScoped(typeof(IRepository<>),typeof(EFRepository<>));
 
+builder.Services.AddScoped<IBasketService, BasketService>();
+builder.Services.AddScoped<IBasketViewModelService, BasketViewModelService>();
 builder.Services.AddScoped<IHomeViewModelService, HomeViewModelService>();
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -25,8 +28,10 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<AppIdentityDbContext>();
-
+                                                                            
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
